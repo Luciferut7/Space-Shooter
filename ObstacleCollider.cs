df22f43f5dc61d18eb9e79ef93053532329b2cce
Luciferut7 <60880097+Luciferut7@users.noreply.github.com>
2023-10-10 17:47:54 +0530

    Create ObstacleCollider.cs

---
+++ ObstacleCollider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollider : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Transform spawnPoint;
+
+    void Start()
+    {
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        transform.position = spawnPoint.position;
+    }
+
+    void Update()
+    {
+        // Move the obstacle downward
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        // Check if the obstacle is below the screen
+        if (transform.position.y < -10f) // You may adjust this value as needed
+        {
+            Destroy(gameObject); // Destroy the obstacle when it's off-screen
+        }
+       /* SomeClass obj = null;
+        // Attempting to access a property or method on obj will result in the error.
+        obj.SomePropertyOrMethod();*/
+
+    }
+}
