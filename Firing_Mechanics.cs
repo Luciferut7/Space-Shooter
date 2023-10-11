2253590d424c26b91b89ff1dd0131d3ffa4b47ca
Luciferut7 <60880097+Luciferut7@users.noreply.github.com>
2023-10-11 23:26:43 +0530

    Update Firing_Mechanics.cs

--- Firing_Mechanics.cs
+++ Firing_Mechanics.cs
@@ -2,43 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Firing_Mechanism : MonoBehaviour
+public class bullet : MonoBehaviour
 {
-    public GameObject projectilePrefab; // Reference to your projectile prefab
-    public Transform firePoint; // The point from which projectiles are fired
-    public float fireForce = 10f; // The force applied to the projectile
+    public float Speed = .5f;
 
-    private float fireCooldown = 0.5f; // Cooldown time in seconds
-    private float nextFireTime = 0f;
-
-    void Update()
+    private void Update()
     {
-        /* if (Input.GetKeyDown(KeyCode.Space)) // Change KeyCode to the desired input key
-         {
-             ShootProjectile();
-         }*/
-        if (Time.time > nextFireTime && Input.GetKeyDown(KeyCode.Space))
-        {
-
-            ShootProjectile();
-
-
-            nextFireTime = Time.time + fireCooldown;
-        }
+        transform.position += -transform.right * Time.deltaTime * Speed;
     }
 
-    void ShootProjectile()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        // Instantiate the projectile
-        GameObject newProjectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-
-        // Access the rigidbody of the projectile (assuming you have one attached)
-        Rigidbody2D rb = newProjectile.GetComponent<Rigidbody2D>();
-
-        // Apply force to the projectile to make it move
-        if (rb != null)
-        {
-            rb.velocity = transform.up * fireForce; // Fire in the direction of the spaceship's forward (up in local space)
-        }
+        
+        Destroy(gameObject);
     }
+
 }
